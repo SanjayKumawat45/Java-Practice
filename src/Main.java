99011e1033d3c27bb8ca0340b3b0a8ec1d61d3d6
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-31 01:48:44 +0530

    rock paper scissor game

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -36,14 +38,105 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Your Name: ");
-        String name =  input.nextLine();
+//        System.out.print("Enter Your Name: ");
+//        String name =  input.nextLine();
+//
+//        System.out.print("Enter Your Age: ");
+//        int age = input.nextInt();
+//
+//        System.out.println("Hello "+name+"!");
+//        System.out.println("You are "+ age+" years old.");
 
-        System.out.print("Enter Your Age: ");
-        int age = input.nextInt();
+// cart
 
-        System.out.println("Hello "+name+"!");
-        System.out.println("You are "+ age+" years old.");
+//        String Item;
+//        int quantity;
+//        double price;
+//        double total;
+//
+//
+//        System.out.print("Enter Item Name: ");
+//        Item = input.nextLine();
+//
+//        System.out.print("Enter Quantity: ");
+//        quantity = input.nextInt();
+//
+//        System.out.print("Enter Price: ");
+//        price = input.nextDouble();
+//
+//        total = price * quantity;
+//
+//        System.out.println("Total bill\n");
+//        System.out.println("Item: " +Item+ "\nQuantity: "+quantity+"\nTotal Price: " + total);
+
+
+// rock paper scissor game
+
+        Random random = new Random();
+
+        int number = random.nextInt(1,3);
+
+        System.out.println(number);
+
+        System.out.println("Enter one Rock, Paper and Scissor: ");
+        String userInput = input.nextLine();
+        String computerInput = "?";
+
+
+        if(number == 1){
+            computerInput = "rock";
+        } else if (number == 2) {
+            computerInput = "paper";
+        }
+        else if (number == 3) {
+            computerInput = "scissor";
+        }
+
+
+        System.out.println("Battle!");
+        System.out.println("Your choice vs Computer's choice");
+        System.out.println(userInput+" vs "+ computerInput);
+
+
+
+
+        if (Objects.equals(userInput, "Rock") || Objects.equals(userInput, "rock")){
+            if (number == 1){
+                System.out.println("its a draw!");
+
+            }
+            else if (number == 2){
+                System.out.println("Computer won the game!");
+            }
+            else {System.out.println("You won the game!");}
+        } else if (Objects.equals(userInput, "Paper") || Objects.equals(userInput, "paper")){
+
+            if (number == 2){
+                System.out.println("its a draw!");
+                return;
+            }
+            else if (number == 3){
+                System.out.println("Computer won the game!");
+                return;
+            }
+            else {System.out.println("You won the game!");
+            return;}
+
+        } else{
+
+            if (number == 3){
+                System.out.println("its a draw!");
+            }
+            else if (number == 1){
+                System.out.println("Computer won the game!");
+            }
+            else {System.out.println("You won the game!");}
+
+        }
+
+
+
+        input.close();
 
     }
 
