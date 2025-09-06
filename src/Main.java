e166b02332293657e0abe75bfb63e806c17654cd
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-09-06 22:23:40 +0530

    Exception handling

--- src/Main.java
+++ src/Main.java
@@ -247,6 +247,20 @@ public class    Main {
 //        System.out.println(pair.getValue());
 
 
+        int[] numerators = {10,200,30,40};
+        int[] denominators = {1,2,0,4};
+
+        for (int i = 0; i< 10; i++) {
+            try {
+                System.out.println(divide(numerators[i], denominators[i]));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+        System.out.println("Good job :)");
+
+
 
 
 
@@ -261,6 +275,18 @@ public class    Main {
 
         input.close();
 
+    }
+
+    public static int divide(int a, int b){
+        try{
+            return a/b;
+        }catch (ArithmeticException e){
+            System.out.println("Arithmetic exception :(");
+            return -1;
+        }catch (Exception e){
+            System.out.println(e);
+            return -1;
+        }
     }
 
 }
